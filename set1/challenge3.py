b337ff7db39daac808f19df98266e56fcc17d8bc
nbarnes1 <nbarnes@hotmail.com>
2019-02-21 20:40:58 -0500

    s1c3, bytes are annoying

--- set1/challenge3.py
+++ set1/challenge3.py
@@ -1,4 +1,13 @@
+from binascii import *
 from challenge2 import fixedXOR
 
 ciphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-print ("test")
+
+
+
+result = fixedXOR(ciphertext, "c"*len(ciphertext))
+
+print (result)
+print (result[2:])
+print (bytes.fromhex(result[2:]))
+print (b2a_uu(bytes.fromhex(result[2:])))
