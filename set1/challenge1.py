4cad6bf2f62083fe2ad85d84caf81acd00ffc68b
nbarnes1 <nbarnes@hotmail.com>
2019-02-21 19:07:00 -0500

    Finished s1c2

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -1,8 +1,12 @@
 from binascii import *
 
-print ("test")
+def hexToB64(str):
+    encoded = b2a_base64(unhexlify(str))
+    return encoded[:-1]
 
 input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-encoded = b2a_base64(bytes.fromhex(input))
+result = hexToB64(input)
+print (result)
 
-print (encoded)
+
+
---
+++ set1/challenge2.py
@@ -0,0 +1,16 @@
+from binascii import *
+
+def fixedXOR(string1, string2):
+    if(len(string1) == len(string2)):
+        tmp1 = int(string1, 16)
+        tmp2 = int(string2, 16)
+        return hex(tmp1 ^ tmp2)
+    else:
+        print ("Error: Fixed XOR non-matching input size")
+        return
+
+input1 = "1c0111001f010100061a024b53535009181c"
+input2 = "686974207468652062756c6c277320657965"
+
+result = fixedXOR(input1, input2)
+print (result)
